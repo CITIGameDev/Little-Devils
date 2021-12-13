58b96e0ff0732c3094b78dbd1ba80a17ae1de80a
erzhanto <erzhariessfeld@gmail.com>
2021-12-14 00:42:19 +0700

    Files

---
+++ Assets/TriggerAkar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAkar : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public SpawnerAkar spwn;
+    
+
+    private void Start()
+    {
+        
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            spwn.updatetimer();
+        }
+            
+            
+        
+    }
+}
